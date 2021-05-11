9bfad8cc582d8eec325139afc36d72c3f4d2ffd1
Enes Türkmen <45971944+enesturkmen@users.noreply.github.com>
2021-05-11 16:50:40 +0300

    updated

--- oopIntro/src/oopIntro/Main.java
+++ oopIntro/src/oopIntro/Main.java
@@ -4,45 +4,27 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Product product1 = new Product();
-		product1.id = 1;
-		product1.name = "Lenovo";
-		product1.unitPrice = 15000;
-		product1.detail = "16 GB Ram";
+		Product product1 = new Product(1, "Lenovo V15", 16000, "32 Gb Ram", 10);
 		
-		Product product2 = new Product(2, "Lenovo V15", 16000, "32 Gb Ram");
+		Product product2 = new Product();
+		product2.setId(2);
+		product2.setName("Lenovo V14");
+		product2.setDetail("16 Gb Ram");
+		product2.setDiscount(10);
+		product2.setUnitPrice(13500);
 		
-		
-		Product product3 = new Product();
-		product3.id = 3;
-		product3.name = "HP";
-		product3.unitPrice = 10000;
-		product3.detail = "16 GB Ram";
-		
-		Product[] products = {
-				product1, product2, product3
-		};
-		
-		for(Product product : products) {
-			
-			System.out.println(product.name);
-		}
-		
-		
-		System.out.println(products.length);
-		
+		System.out.println(product2.getUnitPriceAfterDiscount());
 		
 		Category category1 = new Category();
-		category1.id = 1;
-		category1.name = "Computer";
 		Category category2 = new Category();
-		category2.id = 2;
-		category2.name = "Home, office";
 		
-		ProductManager productManager = new ProductManager();
-		productManager.addToCart(product1);
+		category1.setId(1);
+		category1.setName("Computer");
+		category2.setId(2);
+		category2.setName("Electronics");
 		
-		
+		System.out.println(category1.getName());
+		System.out.println(category2.getName());
 	}
 
 }
