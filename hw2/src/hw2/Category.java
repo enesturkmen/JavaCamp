71373695f653fa39c9a852db888f86a4cec2f5fa
Enes Türkmen <45971944+enesturkmen@users.noreply.github.com>
2021-05-11 14:50:50 +0300

    Add files via upload

---
+++ hw2/src/hw2/Category.java
@@ -0,0 +1,14 @@
+package hw2;
+
+public class Category {
+	
+	int catId;
+	String catName;
+	
+	public Category(int catId, String catName) {
+		
+		this.catId = catId;
+		this.catName = catName;
+	}
+
+}
---
+++ hw2/src/hw2/Courses.java
@@ -0,0 +1,17 @@
+package hw2;
+
+public class Courses {
+	
+	int courseId;
+	String courseName;
+	String courseInstructor;
+	
+	public Courses(int courseId, String courseName, String courseInstructor) {
+		
+		this.courseId = courseId;
+		this.courseName = courseName;
+		this.courseInstructor = courseInstructor;
+	}
+
+}
+
