ae92bdcbcd3896e7e617fec81adb189d0c1cdfa9
Enes Türkmen <45971944+enesturkmen@users.noreply.github.com>
2021-05-12 15:45:27 +0300

    Add files via upload

---
+++ inheritance/src/inheritance/CustomerManager.java
@@ -0,0 +1,22 @@
+package inheritance;
+
+public class CustomerManager{
+	
+	public void add(Customer customer) {
+		System.out.println(customer.customerNumber + " added.");
+	}
+	
+	public void addMultiple(Customer[] customers) {
+		
+		for(Customer customer : customers) {
+			System.out.println(customer.customerNumber + " added.");
+		}
+	}
+	public void Delete(Customer customer) {
+		
+	}
+	public void Update(Customer customer) {
+		
+	}
+
+}
