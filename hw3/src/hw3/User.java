12b22469bad91528979e2a0e632f0c58b83aebd5
Enes Türkmen <45971944+enesturkmen@users.noreply.github.com>
2021-05-12 15:43:37 +0300

    Add files via upload

---
+++ hw3/src/hw3/User.java
@@ -0,0 +1,42 @@
+package hw3;
+
+public class User {
+	
+	int userId;
+	String userName;
+	String userSurname;
+	String eMail;
+	String password;
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserSurname() {
+		return userSurname;
+	}
+	public void setUserSurname(String userSurname) {
+		this.userSurname = userSurname;
+	}
+	public String geteMail() {
+		return eMail;
+	}
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+	public String getPassword() {
+		return password;
+	}
+
+}
